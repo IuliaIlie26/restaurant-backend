a736234ba8ec818ded2d3c3ab14336f1aa6b06c5
Andrei Enachi <andreienachi84@gmail.com>
2022-07-30 16:26:32 +0300

    Updates 2

---
+++ restaurant-application/src/main/java/com/ibm/restaurant/application/clients/ClientService.java
@@ -0,0 +1,7 @@
+package com.ibm.restaurant.application.clients;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+}
---
+++ restaurant-domain/src/main/java/com/ibm/restaurant/domain/IClientRepository.java
@@ -0,0 +1,4 @@
+package com.ibm.restaurant.domain;
+
+public interface IClientRepository {
+}
